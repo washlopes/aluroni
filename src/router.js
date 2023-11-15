c9b2cf8360fa5a1ebd1547f2adb8a309aea8b250
Washington Luís Pedrosa Lopes <washlopes@gmail.com>
2023-11-15 14:24:26 -0300

    Configuração navegação not found

--- src/router.js
+++ src/router.js
@@ -11,7 +11,7 @@ import NotFound from 'pages/NotFound';
 
 export default function AppRouter() {
     return (
-        <main>
+        <main className='container'>
             <Router>
                 <Menu />
                 <Routes>
